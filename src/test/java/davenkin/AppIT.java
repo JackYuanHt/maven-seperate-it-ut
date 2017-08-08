f55ac6b04d1bea49c28a58925ab5d2a30175bdfd
Yun Teng <davenkin@gmail.com>
2017-08-08 16:30:00 +0800

    maven ut and it

---
+++ src/test/java/davenkin/AppIT.java
@@ -0,0 +1,18 @@
+package davenkin;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppIT {
+
+    @Test
+    public void test() {
+        assertEquals(3, 2);
+    }
+
+
+}
---
+++ src/test/java/davenkin/AppTest.java
@@ -0,0 +1,18 @@
+package davenkin;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    @Test
+    public void test() {
+        assertEquals(1, 2);
+    }
+
+
+}
